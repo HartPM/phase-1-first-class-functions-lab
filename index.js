ecdb0fad4aa051c1fe6d19b93deb5e5f7516536a
Patrick Hart <pathart612@gmail.com>
2022-04-05 17:22:24 -0400

    commit tree 47254c0c22d95e949ae45832a812999cff3e5ddb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(arrayDrivers){
+    let newArray = [arrayDrivers[0], arrayDrivers[1]];
+    return newArray;
+}
+
+const returnLastTwoDrivers = (arrayDrivers) => {
+    let newArray = [arrayDrivers[arrayDrivers.length -2], arrayDrivers[arrayDrivers.length -1]];
+    return newArray;
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnFirstTwoDrivers];
+
+function createFareMultiplier(int){
+    return function(fare, int){
+        return fare * int
+    }
+}
